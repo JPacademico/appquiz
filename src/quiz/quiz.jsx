5e8e9d92a054a83b97637f4d1338953955255232
JPacademico <ximbas@academico.ufs.br>
2024-01-15 17:21:17 -0300

    commitao

---
+++ src/quiz/quiz.jsx
@@ -0,0 +1,54 @@
+import "./style.css";
+import { Perguntas } from "../Data/perguntas";
+import { useState } from "react";
+
+export default function Quizz() {
+  const questions = Perguntas ?? [];
+  const [perguntaAtual, setPerguntaAtual] = useState(0);
+  const [showPontuacao, setShowPontuacao] = useState(false);
+  const [pontos, setPontos] = useState(0)
+
+
+
+      function proximaPergunta(correta){
+        const nextQuestion = perguntaAtual + 1 
+
+        if(correta){
+            setPontos(pontos + 1)
+        }
+
+        if(nextQuestion < questions.length){
+            setPerguntaAtual(nextQuestion)
+        }else{
+            setShowPontuacao(true)
+        }
+    }
+
+
+  return (
+    <div className="container">
+        <h2>DevQuiz</h2>
+    {showPontuacao ? (<div className='pontuacao'>
+            <h1>Parabéns</h1>
+            <span id="resultado-Final">sua pontuação é  <strong>{pontos}</strong>  de  <strong>{questions.length}</strong></span></div>):(
+        <>
+        <div className='infoperguntas'>
+            <div className='contagemPerguntas'>
+                <p id="questao">Pergunta {perguntaAtual + 1}/{questions.length}</p>
+            </div>
+            <div className='pergunta'>{questions[perguntaAtual].pergunta}</div>
+        </div>
+        <div className='resposta'>
+            {questions[perguntaAtual].opcoesResposta.map((opcoesResposta) =>
+            <div className='grupoReposta'>
+                <span>{opcoesResposta.alternativa}</span>
+                <button onClick={() => proximaPergunta(opcoesResposta.correta)}>{opcoesResposta.resposta}</button>
+            </div>)}
+            
+        </div>
+    </>
+    )}
+   </div>    
+  );
+}
+ 
