527975ed859d09f804b32f014133a327c393802a
JPacademico <ximbas@academico.ufs.br>
2024-01-17 16:55:35 -0300

    feito

--- src/Data/perguntas.js
+++ src/Data/perguntas.js
@@ -17,6 +17,36 @@ export const Perguntas  = [
             {resposta:'Santos', correta:false, alternativa:'C)'},
             {resposta:'Maranhão', correta:false, alternativa:'D)'},
         ]
+    },
+
+    {
+        pergunta:'Qual o nome do personagem icônico da série "Breaking Bad"?',
+        opcoesResposta:[
+            {resposta:'Saul Goodman', correta:false, alternativa:'A)'},
+            {resposta:'Jesse Pinkman', correta:false, alternativa:'B)'},
+            {resposta:'Walter White', correta:true, alternativa:'C)'},
+            {resposta:'Airlan', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Onde fica o Egito?',
+        opcoesResposta:[
+            {resposta:'América', correta:false, alternativa:'A)'},
+            {resposta:'Europa', correta:false, alternativa:'B)'},
+            {resposta:'África', correta:true, alternativa:'C)'},
+            {resposta:'Ásia', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Quem foi o primeiro a calcular a circunfência da Terra?',
+        opcoesResposta:[
+            {resposta:'Sócrates', correta:false, alternativa:'A)'},
+            {resposta:'Melandro', correta:false, alternativa:'B)'},
+            {resposta:'Eratóstenes', correta:true, alternativa:'C)'},
+            {resposta:'Pitágoras', correta:false, alternativa:'D)'},
+        ]
     }
     
 ]
